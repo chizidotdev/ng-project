c5edc48eb5c7ffaf2ed391a091d9d9f679553869
chizidotdev <chiziwokoma@gmail.com>
2023-04-01 21:58:23 +0100

    feat: create header components

--- src/app/components/button/button.component.ts
+++ src/app/components/button/button.component.ts
@@ -1,10 +1,16 @@
-import { Component } from '@angular/core';
+import { Component, EventEmitter, Input, Output } from '@angular/core';
 
 @Component({
   selector: 'app-button',
   templateUrl: './button.component.html',
-  styleUrls: ['./button.component.css']
+  styleUrls: ['./button.component.css'],
 })
 export class ButtonComponent {
+  @Input() color!: string;
+  @Input() text!: string;
+  @Output() onClick = new EventEmitter();
 
+  handleClick() {
+    this.onClick.emit();
+  }
 }
--- src/app/components/header/header.component.ts
+++ src/app/components/header/header.component.ts
@@ -7,4 +7,8 @@ import { Component } from '@angular/core';
 })
 export class HeaderComponent {
   title = 'Task Tracker';
+
+  toggleAddTask() {
+    alert('toggle')
+  }
 }
